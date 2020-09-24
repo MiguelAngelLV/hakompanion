1ed7d6f66169720e42d81c218b342cdec78d3c65
inorichi <len@kanade.eu>
2020-09-24 09:38:41 +0200

    Send mqtt messages through channel

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -1,14 +1,21 @@
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import mqtt.PahoMqttClient
-import platform.posix.sleep
 
-fun main(): Unit = runBlocking {
+fun main(): Unit = runBlocking(Dispatchers.Default) {
   Platform.isMemoryLeakCheckerActive = false
 
   val client = PahoMqttClient()
-  val result = client.connect("ip:port", "", "")
+  val result = client.connect("ip", "", "")
   println(result)
 
-  client.subscribe("a_topic")
-  sleep(5)
+  GlobalScope.launch(Dispatchers.Default) {
+    client.messages
+      .onEach { println("Got message $it") }
+      .collect()
+  }
+
+  client.subscribe("zigbee2mqtt/#")
+  delay(10000)
 }
